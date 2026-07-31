c24fa1e9ebaa8ce3e76edda2d74198a501b9bd95
Dreamy-Jatiya <jatiyadreamy@gmail.com>
2026-07-31 12:34:25 +0530

    latest

---
+++ Lab-13/SQLQuery1.sql
@@ -0,0 +1,191 @@
+--From the table STUDENT_INFO and RESULT perform the following queries:  
+--Part – A: 
+--1. Combine information from Student and Result table using cross join (Cartesian product). 
+SELECT *
+FROM STUDENT_INFO , RESULT ;
+
+--2. Perform inner join on Student and Result tables. 
+SELECT NAME ,S.BRANCH ,R.SPI
+FROM STUDENT_INFO AS S 
+INNER JOIN RESULT AS R
+ON S.RNO=R.RNO
+
+--3. Perform the left outer join on Student and Result tables. 
+SELECT * 
+FROM STUDENT_INFO AS S 
+LEFT OUTER JOIN RESULT AS R
+ON S.RNO=R.RNO;
+
+--4. Perform the right outer join on Student and Result tables. 
+SELECT NAME 
+FROM STUDENT_INFO AS S 
+RIGHT OUTER JOIN RESULT AS R
+ON S.RNO=R.RNO;
+
+--OR
+--FOR ALL THE TERMS NAME,BRANCH......
+--IN THIS QUERY ALL OUTPUT IS COMING BECAUSE THERE ARE SAME ROLL NO IN BOTH TILL 106
+--AND IF ANY OF THE TABLE DOESN'T HAVE ANY ROLL NO THEN IT WILL SHOW NULL
+SELECT * 
+FROM STUDENT_INFO AS S 
+LEFT OUTER JOIN RESULT AS R
+ON S.RNO=R.RNO;
+
+--5. Perform the full outer join on Student and Result tables. 
+SELECT * 
+FROM STUDENT_INFO AS S 
+FULL OUTER JOIN RESULT AS R
+ON S.RNO=R.RNO;
+
+--6. Display Rno, Name, Branch and SPI of all students. 
+SELECT S.RNO, S.NAME, S.BRANCH, R.SPI 
+FROM STUDENT_INFO AS S 
+INNER JOIN RESULT AS R
+ON S.RNO=R.RNO;
+
+--7. Display Rno, Name, Branch and SPI of CE branch students only.
+SELECT S.RNO, S.NAME, S.BRANCH, R.SPI 
+FROM STUDENT_INFO AS S 
+INNER JOIN RESULT AS R
+ON S.RNO=R.RNO
+WHERE BRANCH='CE'
+
+--8. Display Rno, Name, Branch and SPI of students other than EC branch. 
+SELECT S.RNO, S.NAME, S.BRANCH, R.SPI 
+FROM STUDENT_INFO AS S 
+INNER JOIN RESULT AS R
+ON S.RNO=R.RNO
+WHERE BRANCH!='EC'
+
+--9. Display Rno, Name and SPI of students whose SPI is greater than 8. 
+SELECT S.RNO, S.NAME, S.BRANCH, R.SPI 
+FROM STUDENT_INFO AS S 
+INNER JOIN RESULT AS R
+ON S.RNO=R.RNO
+WHERE SPI>8
+
+--10. Display Rno, Name and Branch of students whose SPI is less than 8. 
+SELECT S.RNO, S.NAME, S.BRANCH, R.SPI 
+FROM STUDENT_INFO AS S 
+INNER JOIN RESULT AS R
+ON S.RNO=R.RNO
+WHERE SPI<8
+
+--11. Display average result of each branch. 
+SELECT S.BRANCH,AVG(SPI)
+FROM STUDENT_INFO AS S 
+INNER JOIN RESULT AS R
+ON S.RNO=R.RNO
+GROUP BY BRANCH
+
+--12. Display average result of CE and ME branch. 
+SELECT S.BRANCH, AVG(SPI)
+FROM STUDENT_INFO AS S 
+INNER JOIN RESULT AS R
+ON S.RNO=R.RNO
+WHERE BRANCH IN ('CE','ME')
+GROUP BY BRANCH
+
+--13. Display maximum and minimum SPI of each branch. 
+SELECT S.BRANCH, MIN(SPI) AS MINIMUM ,MAX(SPI) AS MAXIMUM
+FROM STUDENT_INFO AS S 
+INNER JOIN RESULT AS R
+ON S.RNO=R.RNO
+GROUP BY BRANCH
+
+--14. Display branch-wise student count in descending order.
+SELECT COUNT(S.RNO) AS NO_OF_STUDENTS, S.BRANCH
+FROM STUDENT_INFO AS S 
+INNER JOIN RESULT AS R
+ON S.RNO=R.RNO
+GROUP BY BRANCH
+ORDER BY BRANCH DESC
+
+--15. Display branch-wise total SPI of students. 
+SELECT S.BRANCH, SUM(R.SPI )
+FROM STUDENT_INFO AS S 
+INNER JOIN RESULT AS R
+ON S.RNO=R.RNO
+GROUP BY BRANCH
+ 
+--Part – B: 
+--16. Display branch-wise number of students having SPI greater than 8. 
+SELECT S.BRANCH, COUNT(S.RNO )
+FROM STUDENT_INFO AS S 
+INNER JOIN RESULT AS R
+ON S.RNO=R.RNO
+WHERE R.SPI>8
+GROUP BY BRANCH
+
+--17. Display branch-wise number of students having SPI less than 8. 
+SELECT S.BRANCH, COUNT(S.RNO )
+FROM STUDENT_INFO AS S 
+INNER JOIN RESULT AS R
+ON S.RNO=R.RNO
+WHERE R.SPI<8
+GROUP BY BRANCH
+
+--18. Display branch-wise average SPI greater than 7. 
+SELECT S.BRANCH, AVG(R.SPI )
+FROM STUDENT_INFO AS S 
+INNER JOIN RESULT AS R
+ON S.RNO=R.RNO
+WHERE R.SPI>7
+GROUP BY BRANCH
+
+--19. Display branches having more than 1 students. 
+SELECT S.BRANCH, COUNT(S.RNO )
+FROM STUDENT_INFO AS S 
+INNER JOIN RESULT AS R
+ON S.RNO=R.RNO
+GROUP BY BRANCH
+HAVING COUNT(S.RNO)>1
+
+--20. Display branches where maximum SPI is greater than 9. 
+SELECT S.BRANCH, MAX(R.SPI)
+FROM STUDENT_INFO AS S 
+INNER JOIN RESULT AS R
+ON S.RNO=R.RNO
+GROUP BY BRANCH
+HAVING MAX(R.SPI)>9
+ 
+--Part – C: 
+--21. Display average result of each branch and sort them in ascending order by SPI. 
+SELECT S.BRANCH ,AVG(R.SPI)
+FROM STUDENT_INFO AS S 
+INNER JOIN RESULT AS R
+ON S.RNO=R.RNO
+GROUP BY BRANCH
+ORDER BY AVG(R.SPI)
+
+--22. Display highest SPI from each branch and sort them in descending order.
+SELECT S.BRANCH ,MAX(R.SPI)
+FROM STUDENT_INFO AS S 
+INNER JOIN RESULT AS R
+ON S.RNO=R.RNO
+GROUP BY BRANCH
+ORDER BY MAX(R.SPI) DESC
+
+--23. Display average result of each branch and sort them in ascending order by SPI.
+SELECT S.BRANCH ,AVG(R.SPI)
+FROM STUDENT_INFO AS S 
+INNER JOIN RESULT AS R
+ON S.RNO=R.RNO
+GROUP BY BRANCH
+ORDER BY AVG(R.SPI)
+
+--24. Display highest SPI from each branch and sort them in descending order. 
+SELECT S.BRANCH ,MAX(R.SPI)
+FROM STUDENT_INFO AS S 
+INNER JOIN RESULT AS R
+ON S.RNO=R.RNO
+GROUP BY BRANCH
+ORDER BY MAX(R.SPI) DESC
+
+--25. Display branches where difference between max and min SPI is greater than 1.
+SELECT S.BRANCH ,MAX(R.SPI)-MIN(R.SPI)
+FROM STUDENT_INFO AS S 
+INNER JOIN RESULT AS R
+ON S.RNO=R.RNO
+GROUP BY BRANCH
+HAVING (MAX(R.SPI)-MIN(R.SPI))>1
